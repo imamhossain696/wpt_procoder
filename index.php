836e84fb266d1a9c3d59965441031b0c6bd87128
imamhossain696 <imamhossain696@gmail.com>
2022-12-05 15:54:44 +0600

    new changes

--- index.php
+++ index.php
@@ -13,7 +13,15 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-    
+    <div id="header_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="logo"></a>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 <?php wp_footer(); ?>
